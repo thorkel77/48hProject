688adb68b396b0dee2b4a1a0862e334ed5865994
thorkel77 <46449323+thorkel77@users.noreply.github.com>
2021-02-23 13:27:35 +0100

    Add files via upload

--- insertion.php
+++ insertion.php
@@ -1,4 +1,9 @@
 <?php
+    
+    require __DIR__. '/html/header.html';
+    require __DIR__. '/html/navbar.html';
+?>
+<?php
     $pdo1 = new PDO("mysql:host=localhost;dbname=48hproject", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
     $pdoStat = $pdo1->prepare('SELECT * FROM images');
@@ -25,13 +30,19 @@ $images = $pdoStat->fetchAll();
             <?php
             foreach ($images as $image):?>
     
-            <li>
-            <?=$image['Name']?> <?=$image['product']?> <?=$image['Human']?>
-            </li>
-        </ul>
+        <li>
+            <?=$image['Name']?> <?=$image['Type']?> <?=$image['Image']?>
+        </li>
+     </ul>
     
         <?php endforeach?>
     </ul>
 
 </body>
 </html>
+
+<?php
+    
+    require __DIR__. '/html/footer.html';
+    
+?>
